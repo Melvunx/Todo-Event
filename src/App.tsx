165adb44eb894fa9d9b6be83f5c1ee09c16764bc
Melvunx <augustine.melv@gmmail.com>
2024-10-10 11:54:57 +0200

    eslint tailwind setup

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,7 @@
 function App() {
   return (
-    <div className="bg-zinc-200 w-full">
-      <h1 className="text-2xl font-bold text-center underline">
+    <div className="w-full bg-zinc-200 bg-opacity-10 p-8 text-zinc-950">
+      <h1 className="text-center text-2xl font-bold underline">
         Hello, World!
       </h1>
     </div>
