bcf9c25a7776d930cafa2a45401619a86824727d
Melvunx <augustine.melv@gmmail.com>
2024-10-16 23:03:37 +0200

    path initialise

--- vite.config.ts
+++ vite.config.ts
@@ -1,14 +1,18 @@
-import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 import path from "path";
+import { defineConfig } from "vite";
 
 // https://vitejs.dev/config/
 export default defineConfig({
+  plugins: [react()],
+  base: "Todo-Event",
   resolve: {
     alias: {
       "@": path.resolve(__dirname, "./src"),
       "@assets": path.resolve(__dirname, "./src/assets"),
+      "@components": path.resolve(__dirname, "./src/components"),
+      "@layout": path.resolve(__dirname, "./src/components/layout"),
+      "@pages": path.resolve(__dirname, "./src/pages"),
     },
   },
-  plugins: [react()],
 });
